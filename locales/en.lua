2ab25d1945091fc04756b67a7c0d0cd97b245ed0
xxkellyxx64-pixel <xxkellyxx64@gmail.com>
2025-08-29 23:01:37 +0100

    Add files via upload

--- locales/en.lua
+++ locales/en.lua
@@ -1,11 +1,1 @@
-localesEN = {
-    ["hack_vault"] = "Hack the vault door",
-    ["hack_vault_name"] = "Vault Door",
-    ["vault_hacked"] = "Vault Door hacked successfully!",
-    ["hack_door_name"] = "Door",
-    ["door_hacked"] = "Door hacked successfully!",
-    ["grab_cash"] = "Grab money stacks",
-    ["vault_empty"] = "The security is under alert. Comeback later.",
-    ["hack_failed_name"] = "Laptop",
-    ["hack_failed"] = "Hack failed.",
-}
+no
